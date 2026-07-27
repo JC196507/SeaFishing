3a36ac913b64ef9024096f5b4a96febda7960079
JC196507 <josealvcunha@gmail.com>
2026-07-27 19:20:26 +0100

    Add files via upload

---
+++ locais.js
@@ -0,0 +1,31 @@
+const concelhosLocais = [
+ 	{
+        concelho: "Figueira da Foz",
+        locais: ["Alto do Viso", "Buarcos", "Cabedelo", "Cabo Mondego", "Costa Lavos", "Costinha", "Cova Gala", "Hospital", "Leirosa", "Murtinheira", "Pedra da Nau", "Quiaios"]
+    },
+	{
+        concelho: "Grândola",
+        locais: ["Aberta Nova", "Carvalhal", "Comporta", "Galé", "Melides", "Pêgo", "Pinheirinho", "Raposa", "Santo André"]
+    },
+	{
+        concelho: "Ílhavo, Vagos, Mira e Cantanhede",
+        locais: ["Almadoiros", "Areão", "Praia Barra", "Costa Nova", "Duna Alta", "Gafanha Boa Hora", "Labrego", "Mira", "Palheirão", "Poço da Cruz", "Tocha", "Vagueira"]
+    },
+	{
+        concelho: "Ovar, Murtosa e Aveiro",
+        locais: ["Bico Espanhola", "Califa", "Carreira Tiro", "Furadouro Norte", "Furadouro Sul", "Maceda", "Maceda barro", "Marilia", "Marretas", "Molho Norte","Orbitur", "Ramada", "Ria", "Rubirosa", "Rubirosa norte", "S.Jacinto", "Tricana", "Viveiros"]
+    },
+	{                                                                                                                                                                                                                                                                                          
+        concelho: "Porto e Matosinhos",
+        locais: ["Agudela", "Aldeia Nova", "Aterro", "Boa Nova", "Cabo Mundo", "Corgo", "Facho", "Foz", "Fuzelhas", "Homem do Leme", "Marizé", "Marreco", "Molhe"]
+    },
+	{
+        concelho: "Vila do Conde e Póvoa",
+        locais: ["Aguçadoura", "Atlântica", "Aver-o-Mar", "Caxinas", "Estela", "Labruje", "Leixão", "Mar e Sol", "Mindelo", "Pedras Negras", "Moreiró", "Praia Barcos", "Prainha", "Quião", "Senhor dos Navegantes", "Vila Chã"]
+    },
+	{
+        concelho: "Vila Nova de Gaia e Espinho",
+        locais: ["Aguda", "Barroso", "Bloqueira-Vld", "Canide", "Francelos", "Granja", "Miramar", "Paramos", "Piriri", "Rio Douro", "Silvalde", "Valadares", "Vibrações"]
+    },
+  ];
+  
---
+++ utils.js
@@ -0,0 +1,8 @@
+// utils.js
+// Nota: este ficheiro está intencionalmente vazio.
+// As funções exibirMensagem(), carregarDados() e carregarAnos() que aqui existiam
+// duplicavam (com comportamento diferente) as mesmas funções definidas em script.js.
+// Como script.js carrega depois deste ficheiro, as suas versões sempre prevaleciam
+// e as daqui nunca chegavam a ser usadas. Mantém-se o ficheiro (e a sua inclusão
+// no index.html) para não partir o service worker/cache, mas o código foi removido
+// para evitar duas fontes de verdade divergentes.
