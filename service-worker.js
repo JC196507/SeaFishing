2bd89d06518e49a8888aa8b2e000a71c1a8fd291
JC196507 <josealvcunha@gmail.com>
2026-07-29 20:33:55 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+// service-worker.js
+const CACHE_NAME = 'SeaFishing-cache-v10';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'base.css',
+    'layout.css',
+    'forms.css',
+    'tables.css',
+    'print.css',
+    'responsive.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+    'manifest.json',
+    'ícone/icon-192.png',
+    'ícone/icon-512.png',
+    'utils.js',
+    'resumo_local.js',
+    'print1.js',
+    'print2.js',
+    'print3.js',
+    'print4.js',
+    'script.js',
+    'locais.js',
+    'analise_avancada.js',
+    'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(
+        nomes.filter((nome) => nome !== CACHE_NAME).map((nome) => caches.delete(nome))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
